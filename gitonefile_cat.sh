8f00c4f6dc16a7315ccff3fb79c1bd3246481486
mark <mark>
2009-07-29 03:50:01 +0000

    Initial revision

---
+++ gitonefile_cat.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#
+# checkinonefile.sh
+#
+
+FILENAME="$1"
+if [[ -z $FILENAME ]] ; then echo please supply filename ; exit ; fi
+export GIT_DIR="$FILENAME.git"
+
+# if repo doesn't exist, bail
+if [[ ! -e $GIT_DIR ]] ; then
+    echo "$GIT_DIR" does not exist.  exiting.
+    exit
+fi
+
+git show master:"$FILENAME"
+
+# vim:sw=4:et:ai:ic
